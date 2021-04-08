59663a13c338c1339d6a3938f2f70ad4238d6d03
Anoop Kurungadam <anoop@earthianslive.com>
2021-04-08 09:00:05 +0530

    fix: medication or item_code in prescription, clean up medication

--- healthcare/healthcare/doctype/patient_encounter/patient_encounter.py
+++ healthcare/healthcare/doctype/patient_encounter/patient_encounter.py
@@ -13,6 +13,7 @@ from frappe.utils import add_days, getdate
 class PatientEncounter(Document):
 	def validate(self):
 		self.set_title()
+		self.validate_medications()
 
 	def on_update(self):
 		if self.appointment:
@@ -108,6 +109,11 @@ class PatientEncounter(Document):
 		if plan_item.type == "Therapy Type":
 			self.append("therapies", {"therapy_type": plan_item.template})
 
+	def validate_medications(self):
+		if self.drug_prescription:
+			for item in self.drug_prescription:
+				if not item.medication and not item.drug_code:
+					frappe.throw(_('Error: <b>Drug Prescription</b> Row #{} Medication or Item Code is mandatory').format(item.idx))
 
 @frappe.whitelist()
 def make_ip_medication_order(source_name, target_doc=None):
