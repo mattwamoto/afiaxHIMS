2d9dae9dbb5ff4332fba25f9a95a1c5cdfe95812
Anoop Kurungadam <anoop@earthianslive.com>
2021-12-23 10:25:43 +0530

    fix: inconsistent identifiers and field names after renaming docs corrected removed renamed insurance subscription test

--- healthcare/healthcare/doctype/patient/patient_dashboard.py
+++ healthcare/healthcare/doctype/patient/patient_dashboard.py
@@ -39,7 +39,7 @@ def get_data():
 				"items": ["Inpatient Medication Order"],
 			},
 			{
-				"label": _("Subscriptions"),
+				"label": _("Insurance Policies"),
 				"items": ["Patient Insurance Policy"]
 			}
 		]
