ESS-LLP/erpnext

feat: Healthcare Insurance - Doctypes

erpnext/healthcare/doctype/healthcare_insurance_claim/healthcare_insurance_claim.py
@@ -1,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class HealthcareInsuranceClaim(Document):
+	def on_update_after_submit(self):
+		if self.claim_status != 'Pending':
+			update_claim_status_to_service(self)
+		if self.claim_status == 'Invoiced':
+			create_journal_entry_insurance_claim(self)
+
+def update_claim_status_to_service(doc):
+	service_name = frappe.db.exists(doc.service_doctype,
+	{
+		'insurance_claim': doc.name,
+		'claim_status': 'Pending'
+	})
+	if service_name:
+		frappe.db.set_value(doc.service_doctype, service_name, 'claim_status', doc.claim_status)
+
+def create_journal_entry_insurance_claim(self):
+	# create jv
+	sales_invoice = frappe.get_doc('Sales Invoice', self.sales_invoice)
+	insurance_company = frappe.get_doc('Healthcare Insurance Company', self.insurance_company)
+	from erpnext.accounts.party import get_party_account
+	journal_entry = frappe.new_doc('Journal Entry')
+	journal_entry.company = sales_invoice.company
+	journal_entry.posting_date =  self.billing_date
+	accounts = []
+	tax_amount = 0.0
+	accounts.append({
+			'account': get_party_account('Customer', sales_invoice.customer, sales_invoice.company),
+			'credit_in_account_currency': self.coverage_amount,
+			'party_type': 'Customer',
+			'party': sales_invoice.customer,
+			'reference_type': sales_invoice.doctype,
+			'reference_name': sales_invoice.name
+		})
+	accounts.append({
+			'account': insurance_company.insurance_company_receivable_account,
+			'debit_in_account_currency': self.coverage_amount,
+			'party_type': 'Customer',
+			'party': insurance_company.customer,
+		})
+	journal_entry.set('accounts', accounts)
+	journal_entry.save(ignore_permissions = True)
+	journal_entry.submit()
+	frappe.db.set_value('Healthcare Insurance Claim', self.name, 'service_billed_jv', journal_entry.name)
+	self.reload()

erpnext/healthcare/doctype/healthcare_insurance_payment_request/healthcare_insurance_payment_request.js
@@ -1,0 +1,79 @@
+// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Healthcare Insurance Payment Request', {
+	refresh: function(frm) {
+		if(frm.doc.docstatus == 1){
+			frm.add_custom_button(__('Payment'), function() {
+				frm.events.make_payment_entry(frm);
+			});
+		}
+	},
+	from_date: function(frm){
+		get_insurance_claim(frm);
+	},
+	to_date: function(frm){
+		get_insurance_claim(frm);
+	},
+	insurance_company: function(frm){
+		get_insurance_claim(frm);
+	},
+	posting_date_type: function(frm){
+		get_insurance_claim(frm);
+	},
+	make_payment_entry: function(frm) {
+		return frappe.call({
+			method: 'create_payment_entry',
+			doc: frm.doc,
+			callback: function(r) {
+				var doc = frappe.model.sync(r.message);
+				frappe.set_route('Form', doc[0].doctype, doc[0].name);
+			}
+		});
+	}
+});
+var get_insurance_claim = function(frm){
+	frm.doc.healthcare_insurance_payment_request_item = [];
+	if(frm.doc.insurance_company){
+		var args = {'insurance_company': frm.doc.insurance_company}
+		if(frm.doc.from_date){
+			args['from_date'] = frm.doc.from_date
+		}
+		if(frm.doc.to_date){
+			args['to_date'] = frm.doc.to_date
+		}
+		args['posting_date_type'] = frm.doc.posting_date_type
+		frappe.call({
+			'method': 'erpnext.healthcare.doctype.healthcare_insurance_payment_request.healthcare_insurance_payment_request.get_claim_item',
+			args:args,
+			callback: function (data) {
+				if(data.message){
+					data.message.forEach(function(claim){
+						var child_item=frappe.model.add_child(frm.doc, 'Healthcare Insurance Payment Request Item', 'healthcare_insurance_payment_request_item')
+						frappe.model.set_value(child_item.doctype, child_item.name, 'insurance_claim', claim.name);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'patient', claim.patient);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'healthcare_service_type', claim.healthcare_service_type);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'service_template', claim.service_template);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'sales_invoice', claim.sales_invoice);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'discount', claim.discount);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'claim_coverage', claim.coverage);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'claim_amount', claim.coverage_amount);
+					});
+				}
+				frm.refresh_fields('healthcare_insurance_payment_request_item');
+				set_total_Claim_Amount(frm);
+			}
+		});
+	}
+	frm.refresh_fields();
+}
+let set_total_Claim_Amount = function(frm){
+	var total_claim_amount=0;
+	for (var i in frm.doc.healthcare_insurance_payment_request_item) {
+		var item = frm.doc.healthcare_insurance_payment_request_item[i];
+		if(item.claim_amount ){
+			total_claim_amount = total_claim_amount + item.claim_amount
+		}
+	}
+	frappe.model.set_value(frm.doc.doctype, frm.doc.name, 'total_claim_amount', total_claim_amount)
+}

mattwamoto/afiaxHIMS

healthcare/healthcare/doctype/healthcare_insurance_claim/healthcare_insurance_claim.py
@@ -1,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class HealthcareInsuranceClaim(Document):
+	def on_update_after_submit(self):
+		if self.claim_status != 'Pending':
+			update_claim_status_to_service(self)
+		if self.claim_status == 'Invoiced':
+			create_journal_entry_insurance_claim(self)
+
+def update_claim_status_to_service(doc):
+	service_name = frappe.db.exists(doc.service_doctype,
+	{
+		'insurance_claim': doc.name,
+		'claim_status': 'Pending'
+	})
+	if service_name:
+		frappe.db.set_value(doc.service_doctype, service_name, 'claim_status', doc.claim_status)
+
+def create_journal_entry_insurance_claim(self):
+	# create jv
+	sales_invoice = frappe.get_doc('Sales Invoice', self.sales_invoice)
+	insurance_company = frappe.get_doc('Healthcare Insurance Company', self.insurance_company)
+	from healthcare.accounts.party import get_party_account
+	journal_entry = frappe.new_doc('Journal Entry')
+	journal_entry.company = sales_invoice.company
+	journal_entry.posting_date =  self.billing_date
+	accounts = []
+	tax_amount = 0.0
+	accounts.append({
+			'account': get_party_account('Customer', sales_invoice.customer, sales_invoice.company),
+			'credit_in_account_currency': self.coverage_amount,
+			'party_type': 'Customer',
+			'party': sales_invoice.customer,
+			'reference_type': sales_invoice.doctype,
+			'reference_name': sales_invoice.name
+		})
+	accounts.append({
+			'account': insurance_company.insurance_company_receivable_account,
+			'debit_in_account_currency': self.coverage_amount,
+			'party_type': 'Customer',
+			'party': insurance_company.customer,
+		})
+	journal_entry.set('accounts', accounts)
+	journal_entry.save(ignore_permissions = True)
+	journal_entry.submit()
+	frappe.db.set_value('Healthcare Insurance Claim', self.name, 'service_billed_jv', journal_entry.name)
+	self.reload()

healthcare/healthcare/doctype/healthcare_insurance_payment_request/healthcare_insurance_payment_request.js
@@ -1,0 +1,79 @@
+// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Healthcare Insurance Payment Request', {
+	refresh: function(frm) {
+		if(frm.doc.docstatus == 1){
+			frm.add_custom_button(__('Payment'), function() {
+				frm.events.make_payment_entry(frm);
+			});
+		}
+	},
+	from_date: function(frm){
+		get_insurance_claim(frm);
+	},
+	to_date: function(frm){
+		get_insurance_claim(frm);
+	},
+	insurance_company: function(frm){
+		get_insurance_claim(frm);
+	},
+	posting_date_type: function(frm){
+		get_insurance_claim(frm);
+	},
+	make_payment_entry: function(frm) {
+		return frappe.call({
+			method: 'create_payment_entry',
+			doc: frm.doc,
+			callback: function(r) {
+				var doc = frappe.model.sync(r.message);
+				frappe.set_route('Form', doc[0].doctype, doc[0].name);
+			}
+		});
+	}
+});
+var get_insurance_claim = function(frm){
+	frm.doc.healthcare_insurance_payment_request_item = [];
+	if(frm.doc.insurance_company){
+		var args = {'insurance_company': frm.doc.insurance_company}
+		if(frm.doc.from_date){
+			args['from_date'] = frm.doc.from_date
+		}
+		if(frm.doc.to_date){
+			args['to_date'] = frm.doc.to_date
+		}
+		args['posting_date_type'] = frm.doc.posting_date_type
+		frappe.call({
+			'method': 'healthcare.healthcare.doctype.healthcare_insurance_payment_request.healthcare_insurance_payment_request.get_claim_item',
+			args:args,
+			callback: function (data) {
+				if(data.message){
+					data.message.forEach(function(claim){
+						var child_item=frappe.model.add_child(frm.doc, 'Healthcare Insurance Payment Request Item', 'healthcare_insurance_payment_request_item')
+						frappe.model.set_value(child_item.doctype, child_item.name, 'insurance_claim', claim.name);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'patient', claim.patient);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'healthcare_service_type', claim.healthcare_service_type);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'service_template', claim.service_template);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'sales_invoice', claim.sales_invoice);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'discount', claim.discount);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'claim_coverage', claim.coverage);
+						frappe.model.set_value(child_item.doctype, child_item.name, 'claim_amount', claim.coverage_amount);
+					});
+				}
+				frm.refresh_fields('healthcare_insurance_payment_request_item');
+				set_total_Claim_Amount(frm);
+			}
+		});
+	}
+	frm.refresh_fields();
+}
+let set_total_Claim_Amount = function(frm){
+	var total_claim_amount=0;
+	for (var i in frm.doc.healthcare_insurance_payment_request_item) {
+		var item = frm.doc.healthcare_insurance_payment_request_item[i];
+		if(item.claim_amount ){
+			total_claim_amount = total_claim_amount + item.claim_amount
+		}
+	}
+	frappe.model.set_value(frm.doc.doctype, frm.doc.name, 'total_claim_amount', total_claim_amount)
+}