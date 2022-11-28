akurungadam/erpnext

feat(wip): create service order on encounter submit

erpnext/healthcare/doctype/patient_encounter/patient_encounter.py
@@ -9,6 +9,7 @@ from frappe.model.document import Document
 from frappe.utils import cstr, getdate, add_days
 from frappe import _
 from frappe.model.mapper import get_mapped_doc
+from erpnext.healthcare.utils import make_healthcare_service_order
 
 class PatientEncounter(Document):
 	def validate(self):
@@ -22,6 +23,8 @@ class PatientEncounter(Document):
 	def on_submit(self):
 		if self.therapies:
 			create_therapy_plan(self)
+		update_encounter_medical_record(self)
+		create_healthcare_service_order(self)
 
 	def on_cancel(self):
 		if self.appointment:
@@ -107,3 +110,138 @@ def delete_ip_medication_order(encounter):
 	record = frappe.db.exists('Inpatient Medication Order', {'patient_encounter': encounter.name})
 	if record:
 		frappe.delete_doc('Inpatient Medication Order', record, force=1)
+
+
+def create_healthcare_service_order(encounter):
+	if encounter.drug_prescription:
+		for drug in encounter.drug_prescription:
+			medication = frappe.get_doc('Medication', drug.drug_code)
+			args={
+				'healthcare_service_order_category': medication.get_value('healthcare_service_order_category'),
+				'patient_care_type': medication.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': drug.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Medication',
+				'order': medication.name,
+				'order_description': medication.get_value('description'),
+				'intent': drug.get_value('intent'),
+				'priority': drug.get_value('priority'),
+				'quantity': drug.get_quantity(),
+				'sequence': drug.get_value('sequence'),
+				'expected_date': drug.get_value('expected_date'),
+				'as_needed': drug.get_value('as_needed'),
+				'occurrence': drug.get_value('occurrence'),
+				'staff_role': medication.get_value('staff_role'),
+				'note': drug.get_value('note'),
+				'patient_instruction': drug.get_value('patient_instruction'),
+				'company':encounter.company
+				}
+			make_healthcare_service_order(args)
+	if encounter.lab_test_prescription:
+		for labtest in encounter.lab_test_prescription:
+			lab_template = frappe.get_doc('Lab Test Template', labtest.lab_test_code)
+			args={
+				'healthcare_service_order_category': lab_template.get_value('healthcare_service_order_category'),
+				'patient_care_type': lab_template.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': labtest.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Lab Test Template',
+				'order': lab_template.name,
+				'order_description': lab_template.get_value('lab_test_description'),
+				'quantity' : 1,
+				'intent': labtest.get_value('intent'),
+				'priority': labtest.get_value('priority'),
+				'sequence': labtest.get_value('sequence'),
+				'as_needed': labtest.get_value('as_needed'),
+				'staff_role': lab_template.get_value('staff_role'),
+				'note': labtest.get_value('note'),
+				'patient_instruction': labtest.get_value('patient_instruction'),
+				'healthcare_service_unit_type':lab_template.get_value('healthcare_service_unit_type'),
+				'company':encounter.company
+				}
+			make_healthcare_service_order(args)
+	if encounter.procedure_prescription:
+		for procedure in encounter.procedure_prescription:
+			procedure_template = frappe.get_doc('Clinical Procedure Template', procedure.procedure)
+			args={
+				'healthcare_service_order_category': procedure_template.get_value('healthcare_service_order_category'),
+				'patient_care_type': procedure_template.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': procedure.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Clinical Procedure Template',
+				'order': procedure_template.name,
+				'order_description': procedure_template.get_value('description'),
+				'quantity' : 1,
+				'intent': procedure.get_value('intent'),
+				'priority': procedure.get_value('priority'),
+				'sequence': procedure.get_value('sequence'),
+				'as_needed': procedure.get_value('as_needed'),
+				'body_part': procedure.get_value('body_part'),
+				'staff_role': procedure_template.get_value('staff_role'),
+				'note': procedure.get_value('note'),
+				'patient_instruction': procedure.get_value('patient_instruction'),
+				'healthcare_service_unit_type':lab_template.get_value('healthcare_service_unit_type'),
+				'company':encounter.company
+				}
+			make_healthcare_service_order(args)
+	if encounter.therapies:
+		for therapy in encounter.therapies:
+			therapy_type = frappe.get_doc('Therapy Type', therapy.therapy_type)
+			args={
+				'healthcare_service_order_category': therapy_type.get_value('healthcare_service_order_category'),
+				'patient_care_type': therapy_type.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': therapy.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Therapy Type',
+				'order': therapy_type.name,
+				'order_description': therapy_type.get_value('description'),
+				'quantity' : 1,
+				'intent': therapy.get_value('intent'),
+				'priority': therapy.get_value('priority'),
+				'sequence': therapy.get_value('sequence'),
+				'as_needed': therapy.get_value('as_needed'),
+				'staff_role': therapy_type.get_value('staff_role'),
+				'note': therapy.get_value('note'),
+				'patient_instruction': therapy.get_value('patient_instruction'),
+				'company':encounter.company
+				# 'healthcare_service_unit_type':therapy_type.get_value('healthcare_service_unit_type')
+				}
+			make_healthcare_service_order(args)
+	if encounter.radiology_procedure_prescription:
+		for radiology in encounter.radiology_procedure_prescription:
+			radiology_template = frappe.get_doc('Radiology Examination Template', radiology.radiology_examination_template)
+			args={
+				'healthcare_service_order_category': radiology_template.get_value('healthcare_service_order_category'),
+				'patient_care_type': radiology_template.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': radiology.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Radiology Examination Template',
+				'order': radiology_template.name,
+				'order_description': radiology_template.get_value('description'),
+				'quantity' : 1,
+				'intent': radiology.get_value('intent'),
+				'priority': radiology.get_value('priority'),
+				'sequence': radiology.get_value('sequence'),
+				'as_needed': radiology.get_value('as_needed'),
+				'staff_role': radiology_template.get_value('staff_role'),
+				'note': radiology.get_value('note'),
+				'patient_instruction': radiology.get_value('patient_instruction'),
+				'healthcare_service_unit_type':radiology_template.get_value('healthcare_service_unit_type'),
+				'company':encounter.company
+				}
+			make_healthcare_service_order(args)

mattwamoto/afiaxHIMS

healthcare/healthcare/doctype/patient_encounter/patient_encounter.py
@@ -8,7 +8,7 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
 from frappe.utils import add_days, getdate
-
+from healthcare.healthcare.utils import make_healthcare_service_order
 
 class PatientEncounter(Document):
 	def validate(self):
@@ -22,6 +22,8 @@ class PatientEncounter(Document):
 	def on_submit(self):
 		if self.therapies:
 			create_therapy_plan(self)
+		update_encounter_medical_record(self)
+		create_healthcare_service_order(self)
 
 	def on_cancel(self):
 		if self.appointment:
@@ -190,3 +192,138 @@ def delete_ip_medication_order(encounter):
 	record = frappe.db.exists("Inpatient Medication Order", {"patient_encounter": encounter.name})
 	if record:
 		frappe.delete_doc("Inpatient Medication Order", record, force=1)
+
+
+def create_healthcare_service_order(encounter):
+	if encounter.drug_prescription:
+		for drug in encounter.drug_prescription:
+			medication = frappe.get_doc('Medication', drug.drug_code)
+			args={
+				'healthcare_service_order_category': medication.get_value('healthcare_service_order_category'),
+				'patient_care_type': medication.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': drug.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Medication',
+				'order': medication.name,
+				'order_description': medication.get_value('description'),
+				'intent': drug.get_value('intent'),
+				'priority': drug.get_value('priority'),
+				'quantity': drug.get_quantity(),
+				'sequence': drug.get_value('sequence'),
+				'expected_date': drug.get_value('expected_date'),
+				'as_needed': drug.get_value('as_needed'),
+				'occurrence': drug.get_value('occurrence'),
+				'staff_role': medication.get_value('staff_role'),
+				'note': drug.get_value('note'),
+				'patient_instruction': drug.get_value('patient_instruction'),
+				'company':encounter.company
+				}
+			make_healthcare_service_order(args)
+	if encounter.lab_test_prescription:
+		for labtest in encounter.lab_test_prescription:
+			lab_template = frappe.get_doc('Lab Test Template', labtest.lab_test_code)
+			args={
+				'healthcare_service_order_category': lab_template.get_value('healthcare_service_order_category'),
+				'patient_care_type': lab_template.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': labtest.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Lab Test Template',
+				'order': lab_template.name,
+				'order_description': lab_template.get_value('lab_test_description'),
+				'quantity' : 1,
+				'intent': labtest.get_value('intent'),
+				'priority': labtest.get_value('priority'),
+				'sequence': labtest.get_value('sequence'),
+				'as_needed': labtest.get_value('as_needed'),
+				'staff_role': lab_template.get_value('staff_role'),
+				'note': labtest.get_value('note'),
+				'patient_instruction': labtest.get_value('patient_instruction'),
+				'healthcare_service_unit_type':lab_template.get_value('healthcare_service_unit_type'),
+				'company':encounter.company
+				}
+			make_healthcare_service_order(args)
+	if encounter.procedure_prescription:
+		for procedure in encounter.procedure_prescription:
+			procedure_template = frappe.get_doc('Clinical Procedure Template', procedure.procedure)
+			args={
+				'healthcare_service_order_category': procedure_template.get_value('healthcare_service_order_category'),
+				'patient_care_type': procedure_template.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': procedure.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Clinical Procedure Template',
+				'order': procedure_template.name,
+				'order_description': procedure_template.get_value('description'),
+				'quantity' : 1,
+				'intent': procedure.get_value('intent'),
+				'priority': procedure.get_value('priority'),
+				'sequence': procedure.get_value('sequence'),
+				'as_needed': procedure.get_value('as_needed'),
+				'body_part': procedure.get_value('body_part'),
+				'staff_role': procedure_template.get_value('staff_role'),
+				'note': procedure.get_value('note'),
+				'patient_instruction': procedure.get_value('patient_instruction'),
+				'healthcare_service_unit_type':lab_template.get_value('healthcare_service_unit_type'),
+				'company':encounter.company
+				}
+			make_healthcare_service_order(args)
+	if encounter.therapies:
+		for therapy in encounter.therapies:
+			therapy_type = frappe.get_doc('Therapy Type', therapy.therapy_type)
+			args={
+				'healthcare_service_order_category': therapy_type.get_value('healthcare_service_order_category'),
+				'patient_care_type': therapy_type.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': therapy.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Therapy Type',
+				'order': therapy_type.name,
+				'order_description': therapy_type.get_value('description'),
+				'quantity' : 1,
+				'intent': therapy.get_value('intent'),
+				'priority': therapy.get_value('priority'),
+				'sequence': therapy.get_value('sequence'),
+				'as_needed': therapy.get_value('as_needed'),
+				'staff_role': therapy_type.get_value('staff_role'),
+				'note': therapy.get_value('note'),
+				'patient_instruction': therapy.get_value('patient_instruction'),
+				'company':encounter.company
+				# 'healthcare_service_unit_type':therapy_type.get_value('healthcare_service_unit_type')
+				}
+			make_healthcare_service_order(args)
+	if encounter.radiology_procedure_prescription:
+		for radiology in encounter.radiology_procedure_prescription:
+			radiology_template = frappe.get_doc('Radiology Examination Template', radiology.radiology_examination_template)
+			args={
+				'healthcare_service_order_category': radiology_template.get_value('healthcare_service_order_category'),
+				'patient_care_type': radiology_template.get_value('patient_care_type'),
+				'order_date': encounter.get_value('encounter_date'),
+				'ordered_by': encounter.get_value('practitioner'),
+				'order_group': encounter.name,
+				'replaces': radiology.get_value('replaces'),
+				'patient': encounter.get_value('patient'),
+				'order_doctype': 'Radiology Examination Template',
+				'order': radiology_template.name,
+				'order_description': radiology_template.get_value('description'),
+				'quantity' : 1,
+				'intent': radiology.get_value('intent'),
+				'priority': radiology.get_value('priority'),
+				'sequence': radiology.get_value('sequence'),
+				'as_needed': radiology.get_value('as_needed'),
+				'staff_role': radiology_template.get_value('staff_role'),
+				'note': radiology.get_value('note'),
+				'patient_instruction': radiology.get_value('patient_instruction'),
+				'healthcare_service_unit_type':radiology_template.get_value('healthcare_service_unit_type'),
+				'company':encounter.company
+				}
+			make_healthcare_service_order(args)