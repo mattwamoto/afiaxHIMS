frappe/erpnext

feat: add custom fields for Insurance to Sales Invoice Co-authored-by: muhammadmp <muhammad@earthianslive.com>

erpnext/patches/v13_0/setup_healthcare_service_order_and_insurance.py
@@ -1,0 +1,18 @@
+
+from __future__ import unicode_literals
+import frappe
+from erpnext.healthcare.setup import setup_healthcare_service_order_masters
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from erpnext.domains.healthcare import data
+
+def execute():
+	if "Healthcare" not in frappe.get_active_domains():
+		return
+
+	setup_healthcare_service_order_masters()
+
+	frappe.reload_doc('accounts', 'doctype', 'sales_invoice')
+	frappe.reload_doc('accounts', 'doctype', 'sales_invoice_item')
+
+	if data['custom_fields']:
+		create_custom_fields(data['custom_fields'])

mattwamoto/afiaxHIMS

healthcare/patches/v13_0/setup_healthcare_service_order_and_insurance.py
@@ -1,0 +1,18 @@
+
+from __future__ import unicode_literals
+import frappe
+from healthcare.healthcare.setup import setup_healthcare_service_order_masters
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from erpnext.domains.healthcare import data
+
+def execute():
+	if "Healthcare" not in frappe.get_active_domains():
+		return
+
+	setup_healthcare_service_order_masters()
+
+	frappe.reload_doc('accounts', 'doctype', 'sales_invoice')
+	frappe.reload_doc('accounts', 'doctype', 'sales_invoice_item')
+
+	if data['custom_fields']:
+		create_custom_fields(data['custom_fields'])